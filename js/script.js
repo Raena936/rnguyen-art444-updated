4af76dab5dad6e497381ef4207cc9353e70a64b1
Raena936 <rnguyen5093@sdsu.edu>
2026-03-26 23:15:53 -0700

    added javascript for hamburger menu
    also added 2 pictures and svg for menu and light mode

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-// ALL YOU JS GOES HERE
+// ALL YOUR JS GOES HERE
+
+//hamburbur menu toggle
+function myFunction() {
+    var x = document.getElementById("mobile-links");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+
+const mainNav = document.getElementById("hamburg-menu");
+const navLink = mainNav.querySelectorAll('.main-nav');
+
+ for (let i = 0; i < navLink.length; i++) {
+     navLink[i].addEventListener('click', myFunction);
+ }
+
+ function alertMessage() {
+     alert("Hey you clicked the message, good job wew.")
+}
